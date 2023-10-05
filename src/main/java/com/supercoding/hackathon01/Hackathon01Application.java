b591756897df9d8ae65fc1afd26e999e31131ee1
no.oneho <oneho.dev@gmail.com>
2023-10-06 02:27:25 +0900

    Init

---
+++ src/main/java/com/supercoding/hackathon01/Hackathon01Application.java
@@ -0,0 +1,13 @@
+package com.supercoding.hackathon01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hackathon01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/supercoding/hackathon01/Hackathon01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supercoding.hackathon01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

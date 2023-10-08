e905a8343844e0ac3a0db37e5b39f399b76feb9b
no.oneho <oneho.dev@gmail.com>
2023-10-08 21:06:46 +0900

    Feat: 구매, 판매현황 상태 개수 API 추가

--- src/main/java/com/supercoding/hackathon01/repository/transaction/TransactionRepository.java
+++ src/main/java/com/supercoding/hackathon01/repository/transaction/TransactionRepository.java
@@ -3,6 +3,7 @@ package com.supercoding.hackathon01.repository.transaction;
 import com.supercoding.hackathon01.dto.my_page.response.HomeListResponse;
 import com.supercoding.hackathon01.dto.my_page.response.PurchaseListResponse;
 import com.supercoding.hackathon01.dto.my_page.response.SellingListResponse;
+import com.supercoding.hackathon01.dto.my_page.response.StatusCountResponse;
 import com.supercoding.hackathon01.entity.Transaction;
 import com.supercoding.hackathon01.entity.User;
 import org.springframework.data.domain.Page;
@@ -10,6 +11,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransactionRepository extends TransactionCustomRepository, JpaRepository<Transaction, Long> {
 
@@ -53,5 +56,28 @@ public interface TransactionRepository extends TransactionCustomRepository, JpaR
             "ON p.home = h " +
             "AND p.isThumbnail = true")
     Page<HomeListResponse> findByMyHomeList(@Param("user") User user, Pageable pageable);
+
+    @Query("SELECT new com.supercoding.hackathon01.dto.my_page.response.StatusCountResponse " +
+            "(s.name, COUNT(t)) " +
+            "FROM Status s " +
+            "LEFT JOIN Transaction t " +
+            "ON t.status = s " +
+            "AND t.seller = :user " +
+            "LEFT JOIN User u " +
+            "ON t.seller = u " +
+            "GROUP BY s")
+    List<StatusCountResponse> countTransactionsByStatusForSeller(@Param("user") User user);
+
+    @Query("SELECT new com.supercoding.hackathon01.dto.my_page.response.StatusCountResponse " +
+            "(s.name, COUNT(t)) " +
+            "FROM Status s " +
+            "LEFT JOIN Transaction t " +
+            "ON t.status = s " +
+            "AND t.buyer = :user " +
+            "LEFT JOIN User u " +
+            "ON t.buyer = u " +
+            "GROUP BY s")
+    List<StatusCountResponse> countTransactionsByStatusForBuyer(@Param("user") User user);
+
 
 }
